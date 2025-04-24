88d64f3dec3bf7bcf83a0481edac98db5c1986ef
Evelina Spasova <spasova.evelina@gmail>
2025-04-25 00:31:19 +0300

    Updated App

--- The-Barrens-Blog/src/App.jsx
+++ The-Barrens-Blog/src/App.jsx
@@ -4,6 +4,11 @@ import { AuthProvider } from './contexts/AuthContext';
 import Navbar from './components/Navbar';
 import SignUpPage from './components/SignUpPage';
 import LoginPage from './components/LoginPage';
+import HeroSection from './components/HeroSection';
+import Footer from './components/Footer';
+import BlogSection from './components/BlogSection';
+import CreatePostPage from './components/CreatePostPage';
+import BlogDetail from './components/BlogDetail';
 
 function App() {
   return (
@@ -13,7 +18,16 @@ function App() {
         <Routes>
           <Route path='/signup' element={<SignUpPage />} />
           <Route path='/login' element={<LoginPage />} />
+          <Route path='/blog/:id' element={<BlogDetail />} />
+          <Route path='/admin/create-post' element={<CreatePostPage/>} />
+          <Route path="/" element={
+            <>
+              <HeroSection />
+              <BlogSection />
+            </>
+          } />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </AuthProvider>
   )
