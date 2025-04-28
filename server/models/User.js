f857aabd9ef6ee13c91b2856e1238892148f6140
Evelina Spasova <spasova.evelina@gmail>
2025-04-28 23:43:04 +0300

    Added username validation

--- server/models/User.js
+++ server/models/User.js
@@ -5,6 +5,7 @@ const UserSchema = new mongoose.Schema({
   username: {
     type: String,
     required: [true, 'Please provide a username'],
+    minlength: [3, 'Username must be at least 3 characters'],
     unique: true,
     trim: true,
   },
