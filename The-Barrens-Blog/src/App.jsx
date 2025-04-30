7a872b4752303b7293d53ba59a79910bc136c7c6
Evelina Spasova <spasova.evelina@gmail>
2025-04-30 03:34:07 +0300

    Added routes to the App

--- The-Barrens-Blog/src/App.jsx
+++ The-Barrens-Blog/src/App.jsx
@@ -1,14 +1,16 @@
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider } from './contexts/AuthContext';
-import Navbar from './components/Navbar';
-import SignUpPage from './components/SignUpPage';
-import LoginPage from './components/LoginPage';
-import HeroSection from './components/HeroSection';
-import Footer from './components/Footer';
-import BlogSection from './components/BlogSection';
-import CreatePostPage from './components/CreatePostPage';
-import BlogDetail from './components/BlogDetail';
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { AuthProvider } from "./contexts/AuthContext";
+import Navbar from "./components/Navbar";
+import SignUpPage from "./components/SignUpPage";
+import LoginPage from "./components/LoginPage";
+import HeroSection from "./components/HeroSection";
+import Footer from "./components/Footer";
+import BlogSection from "./components/BlogSection";
+import CreatePostPage from "./components/CreatePostPage";
+import BlogDetail from "./components/BlogDetail";
+import AdminPostPage from "./components/AdminPostsPage";
+import EditPostForm from "./components/EditPostForm";
 
 function App() {
   return (
@@ -16,21 +18,26 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path='/signup' element={<SignUpPage />} />
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/blog/:id' element={<BlogDetail />} />
-          <Route path='/admin/create-post' element={<CreatePostPage/>} />
-          <Route path="/" element={
-            <>
-              <HeroSection />
-              <BlogSection />
-            </>
-          } />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/blog/:id" element={<BlogDetail />} />
+          <Route path="/admin/create-post" element={<CreatePostPage />} />
+          <Route path="/admin/manage-posts" element={<AdminPostPage />} />
+          <Route path="/admin/edit-post/:id" element={<EditPostForm />} />
+          <Route
+            path="/"
+            element={
+              <>
+                <HeroSection />
+                <BlogSection />
+              </>
+            }
+          />
         </Routes>
         <Footer />
       </BrowserRouter>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
