d52be82833df93f56f9358619c06b0cf6fab2d1d
Evelina Spasova <spasova.evelina@gmail.com>
2025-05-02 23:34:29 +0300

    Update App routes

--- The-Barrens-Blog/src/App.jsx
+++ The-Barrens-Blog/src/App.jsx
@@ -11,30 +11,51 @@ import CreatePostPage from "./components/CreatePostPage";
 import BlogDetail from "./components/BlogDetail";
 import AdminPostPage from "./components/AdminPostsPage";
 import EditPostForm from "./components/EditPostForm";
+import AuthStateWrapper from "./components/common/AuthStateWrapper";
+import {
+  AdminRoute,
+  ProtectedRoute,
+  PublicOnlyRoute,
+} from "./components/guards/ProtectedRoute";
+import ProfilePage from "./components/ProfilePage";
+import NotFoundPage from "./components/NotFoundPage";
 
 function App() {
   return (
     <AuthProvider>
       <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route path="/signup" element={<SignUpPage />} />
-          <Route path="/login" element={<LoginPage />} />
-          <Route path="/blog/:id" element={<BlogDetail />} />
-          <Route path="/admin/create-post" element={<CreatePostPage />} />
-          <Route path="/admin/manage-posts" element={<AdminPostPage />} />
-          <Route path="/admin/edit-post/:id" element={<EditPostForm />} />
-          <Route
-            path="/"
-            element={
-              <>
-                <HeroSection />
-                <BlogSection />
-              </>
-            }
-          />
-        </Routes>
-        <Footer />
+        <AuthStateWrapper>
+          <Navbar />
+          <Routes>
+            <Route path="/" element={
+                <>
+                  <HeroSection />
+                  <BlogSection />
+                </>
+              }
+            />
+            <Route path="/blog/:id" element={<BlogDetail />} />
+
+            <Route element={<PublicOnlyRoute />}>
+              <Route path="/signup" element={<SignUpPage />} />
+              <Route path="/login" element={<LoginPage />} />
+            </Route>
+
+            <Route element={<ProtectedRoute />}>
+              <Route path="/profile" element={<ProfilePage />} />
+            </Route>
+
+            <Route element={<AdminRoute />}>
+              <Route path="/admin/create-post" element={<CreatePostPage />} />
+              <Route path="/admin/manage-posts" element={<AdminPostPage />} />
+              <Route path="/admin/edit-post/:id" element={<EditPostForm />} />
+            </Route>
+
+            <Route path="*" element={<NotFoundPage />} />
+
+          </Routes>
+          <Footer />
+        </AuthStateWrapper>
       </BrowserRouter>
     </AuthProvider>
   );
